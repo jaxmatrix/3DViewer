07087f353f9b30d033c1fe2500b35de134724ab0
Jai Shukla <bio.sensor.noice@gmail.com>
2023-01-27 21:47:46 +0530

    Basic project setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { Viewer3dModule } from './viewer3d/viewer3d.module';
+import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ViewportComponent } from './viewport/viewport.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewportComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    Viewer3dModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/viewport/viewport.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewport',
+  templateUrl: './viewport.component.html',
+  styleUrls: ['./viewport.component.scss']
+})
+export class ViewportComponent {
+
+}
